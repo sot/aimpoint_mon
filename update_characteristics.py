0863e2599daf4edb7ad1050e976ff87d51d1e0b8
Tom Aldcroft <taldcroft@gmail.com>
2015-10-11 21:15:46 -0400

    Add summary index file

--- update_characteristics.py
+++ update_characteristics.py
@@ -10,9 +10,11 @@ import os
 from datetime import datetime
 import argparse
 import json
+import copy
 
 import numpy as np
 from astropy.time import Time
+from astropy.table import Table
 from kadi import occweb
 from kadi.events import scrape
 from bs4 import BeautifulSoup as parse_html
@@ -35,6 +37,9 @@ version = None
 
 
 def get_opt(args=None):
+    """
+    Get runtime options
+    """
     parser = argparse.ArgumentParser(description='Create an updated characterstics, '
                                      'place file on lucky, and send notification emails')
     parser.add_argument("--data-root",
@@ -64,6 +69,10 @@ def make_updated_characteristics(baseline_file,
     """
     process_time = Time.now()
 
+    # Calculate new SI_ALIGN matrices based on supplied aimpoint offsets
+    si_align_acis_i = calc_si_align(dy_acis_i / 3600, dz_acis_i / 3600.)
+    si_align_acis_s = calc_si_align(dy_acis_s / 3600., dz_acis_s / 3600.)
+
     logger.info('Reading baseline characteristics {}'.format(baseline_file))
     with open(baseline_file, 'r') as fh:
         lines = fh.readlines()
@@ -73,11 +82,7 @@ def make_updated_characteristics(baseline_file,
     if len(matches) != 1:
         raise ValueError('parsing error, matched {} instances of ODB_SI_ALIGN instead of one'
                          .format(len(matches)))
-
     start = matches[0]
-
-    si_align_acis_i = calc_si_align(dy_acis_i / 3600, dz_acis_i / 3600.)
-    si_align_acis_s = calc_si_align(dy_acis_s / 3600., dz_acis_s / 3600.)
 
     comments = ['!Updated via aimpoint_mon/update_characteristics version {}'.format(version),
                 '!Run at {}Z'.format(process_time.iso[:16]),
@@ -88,6 +93,7 @@ def make_updated_characteristics(baseline_file,
     fmt_first = '       ODB_SI_ALIGN     = {:.6e}, {:.6e}, {:.6e},'
     fmt_other = '                          {:.6e}, {:.6e}, {:.6e},'
 
+    # Make updated lines in characteristics
     for i, vals in enumerate(np.concatenate([si_align_acis_i, si_align_acis_s])):
         fmt = fmt_first if i == 0 else fmt_other
         out = fmt.format(*vals).upper()
@@ -96,28 +102,66 @@ def make_updated_characteristics(baseline_file,
 
         lines[i + start] = out + os.linesep
 
+    # Write the new file into the characteristics/ directory
     filename = 'CHARACTERIS_{}'.format(process_time.datetime.strftime('%d%b%g').upper())
     updated_file = os.path.join(opt.data_root, 'characteristics', filename)
     logger.info('Writing updated characteristics {}'.format(updated_file))
     with open(updated_file, 'w') as fh:
         fh.writelines(lines)
 
-    # Store processing information in a JSON file
-    info = {'date': process_time.iso,
-            'dy_acis_i': dy_acis_i,
-            'dz_acis_i': dz_acis_i,
-            'dy_acis_s': dy_acis_s,
-            'dz_acis_s': dz_acis_s,
-            'dy_dz_units': 'arcsec',
-            'baseline_file': os.path.basename(baseline_file),
-            'updated_file': os.path.basename(updated_file),
-            'si_align_acis_i': si_align_acis_i.tolist(),
-            'si_align_acis_s': si_align_acis_s.tolist(),
-            'version': version,
-            }
+    # Info fields for summary index table
+    info_table = {'date': process_time.yday[:17],
+                  'dy_acis_i': dy_acis_i,
+                  'dz_acis_i': dz_acis_i,
+                  'dy_acis_s': dy_acis_s,
+                  'dz_acis_s': dz_acis_s,
+                  'baseline_file': os.path.basename(baseline_file),
+                  'updated_file': os.path.basename(updated_file),
+                  }
 
+    # More detailed info data for JSON file
+    info_json = copy.copy(info_table)
+    info_json.update({'date_iso': process_time.isot[:19],
+                      'dy_dz_units': 'arcsec',
+                      'si_align_acis_i': si_align_acis_i.tolist(),
+                      'si_align_acis_s': si_align_acis_s.tolist(),
+                      'version': version,
+                      })
+
+    # Make a new JSON file with udpate details
     with open(updated_file + '.json', 'w') as fh:
-        json.dump(info, fh, indent=4, sort_keys=True, separators=(',', ': '))
+        json.dump(info_json, fh, indent=4, sort_keys=True, separators=(',', ': '))
+
+    return info_table
+
+
+def write_index_file(info_table):
+    """
+    Write a summary index file in tabular format from ``info_table`` dict, e.g.
+
+        updated_file       baseline_file           date       dy_acis_i dz_acis_i dy_acis_s dz_acis_s
+    ------------------- ------------------- ----------------- --------- --------- --------- ---------
+    CHARACTERIS_12OCT15 CHARACTERIS_12MAR15 2015:285:01:21:25     -5.00     10.00     15.00    -20.00
+    """
+    index_file = os.path.join(opt.data_root, 'characteristics', 'index')
+    if os.path.exists(index_file):
+        index = Table.read(index_file, format='ascii.fixed_width_two_line', guess=False)
+        matching = index['updated_file'] == info_table['updated_file']
+        index = index[~matching]
+        if np.any(matching):
+            logger.info('WARNING: replacing existing entry for updated_file={}'
+                        .format(info_table['updated_file']))
+    else:
+        index = Table(names=['updated_file', 'baseline_file', 'date',
+                             'dy_acis_i', 'dz_acis_i', 'dy_acis_s', 'dz_acis_s'],
+                      dtype=['S19', 'S19', 'S17', 'f', 'f', 'f', 'f'])
+    index.add_row(info_table)
+    for colname in ('dy_acis_i', 'dz_acis_i', 'dy_acis_s', 'dz_acis_s'):
+        index[colname].format = '.2f'
+
+    logger.info('Writing index file {}'.format(index_file))
+    with open(index_file, 'w') as fh:
+        fh.writelines(line + os.linesep for line in index.pformat())
 
 
 def get_baseline_characteristics_file():
@@ -177,7 +221,8 @@ def main():
     version = open(os.path.join(opt.data_root, 'VERSION'), 'r').read().strip()
 
     baseline_file = get_baseline_characteristics_file()
-    make_updated_characteristics(baseline_file)
+    info = make_updated_characteristics(baseline_file)
+    write_index_file(info)
 
 
 if __name__ == '__main__':
